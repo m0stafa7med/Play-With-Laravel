b6fa9df4a3615fe70910bbd99129ecde05a30fd0
m0stafa7med <mostafa.ahmed1093@yahoo.com>
2020-08-20 01:18:17 +0200

    sixth commit

---
+++ app/Models/Doctor.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Doctor extends Model
+{
+    protected $table="doctors";
+    protected $fillable=['name','title','hospital_id','created_at','updated_at'];
+    protected $hidden=['created_at','updated_at'];
+      // public $timestamps=false;
+
+    public function hospital()
+    {
+        return $this->belongsTo('App\Models\Hospital','hospital_id','id');
+    }
+}
